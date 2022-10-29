8bba35afcca773580c14b2cecbc997e78c14d200
L1ttleWizard <94560090+L1ttleWizard@users.noreply.github.com>
2022-10-29 09:32:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 getData();
 
+ 
+async function getData() {
  const removeEmptyOrNull = (obj) => {
       Object.keys(obj).forEach(k =>
         (obj[k] && typeof obj[k] === 'object') && removeEmptyOrNull(obj[k]) ||
@@ -7,13 +9,15 @@ getData();
       );
       return obj;
     };
-async function getData() {
     const response = await fetch('https://esp-32-demo-f34e1-default-rtdb.europe-west1.firebasedatabase.app/test.json');
     const data = await response.json();
-    
+    console.log(data);
+ console.log('data with null);
     removeEmptyOrNull(data);
+ console.log('data without null');
     data.sort((a, b) => new Date(a["date_time"].slice(0, -1)) - new Date(b["date_time"].slice(0, -1)));
     console.log(data);
+ console.log('data with sort');
     length = data.length;
     console.log(length);
     labels = [];
